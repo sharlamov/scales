8c3d730b8846759de46d778ba11a88a550d60b26
sharlamov <syharlamov@gmail.com>
2015-07-13 11:31:39 +0300

    locale change

--- src/main/webapp/resources/js/common.js
+++ src/main/webapp/resources/js/common.js
@@ -8,3 +8,31 @@ function calcNetto() {
 function setDialogName(dialogName) {
 	document.getElementById("formDlg1:editDialog_title").innerHTML = dialogName;
 }
+
+
+PrimeFaces.locales ['ro'] = {
+	    closeText: 'Închide',
+	    prevText: 'Înapoi',
+	    nextText: 'Înainte',
+	    currentText: 'Acasă',
+	    monthNames: ['Ianuarie', 'Februarie', 'Martie', 'Aprilie', 'Mai', 'Iunie', 'Iulie', 'August', 'Septembrie', 'Octombrie', 'Noiembrie', 'Decembrie' ],
+	    monthNamesShort: ['Ian', 'Feb', 'Mar', 'Apr', 'Mai', 'Iun', 'Iul', 'Aug', 'Sep', 'Oct', 'Noi', 'Dec' ],
+	    dayNames: ['Duminică', 'Luni', 'Marți', 'Miercuri', 'Joi', 'Vineri', 'Sâmbătă'],
+	    dayNamesShort: ['Dum', 'Lun', 'Mar', 'Mie', 'Joi', 'Vin', 'Sâm'],
+	    dayNamesMin: ['D', 'L', 'M', 'Mi', 'J', 'V', 'S'],
+	    weekHeader: 'Săptămâna',
+	    firstDay: 1,
+	    isRTL: false,
+	    showMonthAfterYear: false,
+	    yearSuffix: '',
+	    timeOnlyTitle: 'Numai timp',
+	    timeText: 'Timp',
+	    hourText: 'Ora',
+	    minuteText: 'Minut',
+	    secondText: 'Secunde',
+	    ampm: false,
+	    month: 'Luna',
+	    week: 'Săptămâna',
+	    day: 'Zi',
+	    allDayText: 'Toată ziua'
+	};
